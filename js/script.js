4fa8fe8580ab99709ff476a80675822546adc77d
Insaf <garfievinsaf@gmail.com>
2024-11-22 10:53:28 +0300

    update index.html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 document.querySelector(".burger").addEventListener("click", () => {
     document.querySelector(".nav-links").classList.toggle("nav-links__active");
 });
+
+const box = document.querySelector(".hamburger-menu");
+document.addEventListener('click', (e) => {
+    const click = e.composedPath().includes(box);
+    if(!click)
+        $('.burger').click();
+})
+
+
